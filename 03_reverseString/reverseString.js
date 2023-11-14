6d69a84b1acb322e6890419d08ac01007c594766
Henry Bernardin <henry.bernardin@gmail.com>
2023-11-13 22:26:30 -0500

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+// const reverseString = function(word) { 
+// let str = word.split(); 
+// return str; 
+// };
 
-};
+// console.log(reverseString); 
 
-// Do not edit below this line
-module.exports = reverseString;
+// // Do not edit below this line
+// module.exports = reverseString;
+let wordStr = "hello there";
+let wordArray = wordStr.split("");
+let reverse = wordArray.reverse(); 
+let join = reverse.join(); 
+let newWord = join.replace(); 
+
+console.log(newWord); // ['h', 'e', 'l', 'l', 'o', ' ', 't', 'h', 'e', 'r', 'e']
